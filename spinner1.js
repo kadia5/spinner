fcf69d004de3392179b6017a9ca906ebb01fe8be
kadia5 <kdabraeux@gmail.com>
2021-07-29 02:30:41 +0000

    spinner1.js

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|\n');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/\n');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-\n');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\ \n'); 
+}, 700);
+
+// ... fill in the rest yourself ...
